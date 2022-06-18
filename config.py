5b30eddd9f4393df078fb2e7e84a2665081fd123
de.Beast <litvinov.d.s100@gmail.com>
2022-06-18 07:13:56 +0300

    [setup for prod]

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 settings = {
-    "discord_token": "OTcyNzgwNTA2Nzc1MTg3NTA4.G2DRjI.dhN-qvFYsFwFwOfXlN96ZbaYTQ-_4vqXCin3B8",
+    "discord_token": "NjY2NjU4MTcwNzkzMzYxNDM4.Xh3XzQ.n2B3flFo19nFMyY_ClNY2e2O7Fs",
     "vkadmin_token": "e8541615ae7bd2d819b4b0367f2853ec7293f0e578ef46eda56028b77a97b471676019953f49b29cff384",
     "db_url": "mongodb+srv://deBeast:wL9iXBJ2S1DVFD7R@10b.h95qh.mongodb.net/test",
     "prefix": "++",
-    "room_name": "10B-DEV_room",
+    "room_name": "10B-classroom",
 }
 
 # 10B-DEV_room OTcyNzgwNTA2Nzc1MTg3NTA4.G2DRjI.dhN-qvFYsFwFwOfXlN96ZbaYTQ-_4vqXCin3B8
