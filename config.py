5e416c61c5da2e74eec6967208be3d36a660562b
de.Beast <litvinov.d.s100@gmail.com>
2022-12-30 16:31:48 +0300

    deployment fix

--- config.py
+++ config.py
@@ -1,25 +1,31 @@
-from dotenv import dotenv_values
-
+import os
+import dotenv
 from src.enums import Configuration
 
 
 def get_config(configuration: str = "prod") -> dict[str, str]:
     if "config" not in globals():
-        globals()["config"] = {**dotenv_values(".env")}
+        globals()["config"] = {}
         config: dict = globals()["config"]
 
         conf = Configuration.get_key(configuration)
         match conf:
             case Configuration.DEV:
-                config.update(DISCORD_TOKEN=config.pop("DISCORD_TOKEN-dev"))
                 config["CONFIGURATION"] = Configuration.DEV
+                config["DISCORD_TOKEN"] = os.getenv("DISCORD_TOKEN-dev")
                 config["PREFIX"] = "++"
                 config["ROOM_NAME"] = "10B-DEV_room"
 
             case Configuration.PROD:
-                config.pop("DISCORD_TOKEN-dev")
                 config["CONFIGURATION"] = Configuration.PROD
+                config["DISCORD_TOKEN"] = os.getenv("DISCORD_TOKEN")
                 config["PREFIX"] = "++"
                 config["ROOM_NAME"] = "10B-classroom"
+        config["VKADMIN_TOKEN"] = os.getenv("VKADMIN_TOKEN")
+        config["MONGODB_URL"] = os.getenv("MONGODB_URL")
 
     return globals()["config"].copy()
+
+
+if __name__ == "config":
+    dotenv.load_dotenv(".env")
